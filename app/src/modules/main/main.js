a7f3669a773539c764d0ae620a41106d3d807778
Igor Shkulipa <Igor.Shkulipa@symphony-solutions.eu>
2016-06-03 16:39:47 +0300

    Accounts

--- app/src/modules/main/main.js
+++ app/src/modules/main/main.js
@@ -1,5 +1,31 @@
 define(['knockout'], (ko) => {
     return {
-        title: ko.observable('Main')
+        strings: {
+            home: ko.observable('Sencoiney'),
+            title: ko.observable('Main'),
+            description: ko.observable('Accounts and general information')
+        },
+        accounts: [
+            {
+                type: 'bank',
+                name: 'Account 1',
+                sum: 44555
+            },
+            {
+                type: 'cash',
+                name: 'Account 2',
+                sum: 55566
+            },
+            {
+                type: 'visa',
+                name: 'Account 3',
+                sum: 33444
+            },
+            {
+                type: 'mastercard',
+                name: 'Account 4',
+                sum: 55444
+            },
+        ]
     };
 });
